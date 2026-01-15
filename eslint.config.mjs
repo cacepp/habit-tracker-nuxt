67d10a342340b1af22cc3a7e92ac3aee7e514550
cacepp <iljushin.anton2002@yandex.ru>
2026-01-15 13:44:55 +0300

    feat: Добавлен eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import { createConfigForNuxt } from '@nuxt/eslint-config';
+
+export default createConfigForNuxt({
+  features: {
+    stylistic: { indent: 2, quotes: 'single', semi: true },
+  },
+})
+  .override('nuxt/rules', {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  });
